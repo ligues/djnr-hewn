c0fd0caddf26effa72b780b06abebd90904f2b91
Carlos Ligues <carlos@ligues.com.mx>
2015-07-02 16:58:58 -0500

    Arreglado el tema del SEO

--- header.php
+++ header.php
@@ -4,10 +4,19 @@
         <base href="<?php echo get_site_url(); ?>/" >
         <meta charset="<?php bloginfo( 'charset' ); ?>" /> 
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>JOINERY MASTERCRAFT FABRICATION</title>
+        <title><?php wp_title(''); ?></title>
         <link rel="profile" href="http://gmpg.org/xfn/11" />
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-        <meta name="description" content="<?php bloginfo('description'); ?>" />
+        <?php
+            //Grab the page seo settings
+            $metadesc = get_post_meta($post->ID, '_yoast_wpseo_metadesc', true);
+            $metakeywords = get_post_meta($post->ID, '_yoast_wpseo_metakeywords', true);
+        ?>
+
+        <?php if(!empty($metadesc)) : ?><?php endif ?>
+
+        <?php if(!empty($metakeywords)) : ?><meta name="keywords" content="<?php echo $metakeywords ?>" /><?php endif ?>
+        
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1,minimum-scale=1">
         <script type="text/javascript">var store='<?php echo get_store_status(); ?>';var store_l='<?php echo get_store_status_legend(); ?>';var WP = '<?php echo get_site_url(); ?>';var theme = '<?php echo get_stylesheet_directory_uri() ?>';</script>
         <?php  wp_head();  ?> 
